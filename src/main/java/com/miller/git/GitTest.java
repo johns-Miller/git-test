1fd3cf8ac3a9d1c884a3a9b39437fc62a8d528fc
miller <654819251@qq.com>
2023-03-09 16:11:24 +0800

    first commit

---
+++ src/main/java/com/miller/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.miller.git;
+
+/**
+ * @author: Miller
+ * @date: 2023/3/9 16:05
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
